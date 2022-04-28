b9545d80a6cff351b08a3e4b258b284f52096426
ananthu <begoodananthu@gmail.com>
2022-04-28 15:57:24 +0200

    gitignore updated

---
+++ csgo_config_parser/__init__.py
@@ -0,0 +1,2 @@
+from .config_parser import CsgoConfigParser
+from .video_config_parser import CsgoVideoConfigParser
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='csgo_config_parser',
+    version= '0.0.1',
+    author= 'Ananthu Saseendran',
+    description='A simple parse module to parse csgo congfig and video settings',
+    packages=['csgo_config_parser']
+)
